4613e5014c69e040c5be4682ff2a0d5e66d8487b
Oleg Fedoseev <o.fedoseev@office.ngs.ru>
2016-03-01 09:59:15 +0600

    wip

--- php_spy.c
+++ php_spy.c
@@ -12,29 +12,44 @@ ZEND_GET_MODULE(spy)
 
 void spy_coverage_line_dtor(void *data)
 {
-	spy_coverage_line *line = (spy_coverage_line *) data;
-	free(line);
+    spy_coverage_line *line = (spy_coverage_line *) data;
+    free(line);
 }
 
-spy_coverage_file *spy_coverage_file_ctor(char *filename)
+spy_coverage_file *spy_coverage_file_ctor(const char *filename)
 {
-	spy_coverage_file *file;
+    spy_coverage_file *file;
 
-	file = malloc(sizeof(spy_coverage_file));
-	file->name = strdup(filename);
-	//file->lines = hash_alloc(128, spy_coverage_line_dtor);
-	zend_hash_init(&file->lines, 128, NULL, spy_coverage_line_dtor, 0);
+    file = malloc(sizeof(spy_coverage_file));
+    file->name = strdup(filename);
+    ALLOC_HASHTABLE(file->lines);
+    zend_hash_init(file->lines, 128, NULL, spy_coverage_line_dtor, 0);
 
-	return file;
+    return file;
 }
 
 void spy_coverage_file_dtor(void *data)
 {
-	spy_coverage_file *file = (spy_coverage_file *) data;
+    spy_coverage_file *file = (spy_coverage_file *) data;
 
-	zend_hash_destroy(&file->lines);
-	free(file->name);
-	free(file);
+    zend_hash_destroy(file->lines);
+    free(file->name);
+    free(file);
+}
+
+
+void php_spy_init_globals(zend_spy_globals *spy_g TSRMLS_DC)
+{
+    spy_g->code_coverage          = NULL;
+    spy_g->previous_filename      = "";
+    spy_g->previous_file          = NULL;
+    spy_g->previous_mark_filename = "";
+    spy_g->previous_mark_file     = NULL;
+}
+
+void php_spy_shutdown_globals(zend_spy_globals *spy_g TSRMLS_DC)
+{
+
 }
 
 /* execution redirection functions */
@@ -42,47 +57,50 @@ zend_op_array* (*old_compile_file)(zend_file_handle* file_handle, int type TSRML
 zend_op_array* spy_compile_file(zend_file_handle*, int TSRMLS_DC);
 
 static const zend_function_entry spy_functions[] = {
-  PHP_FE(spy_hello, NULL)
-  PHP_FE_END
+    PHP_FE(spy_hello, NULL)
+    PHP_FE_END
 };
 
 zend_module_entry spy_module_entry = {
-  STANDARD_MODULE_HEADER,
-  "Spy",                       // your extension name
-  spy_functions,               // where you define your functions
-  PHP_MINIT(spy),     // for module initialization
-  PHP_MSHUTDOWN(spy), // for module shutdown process
-  PHP_RINIT(spy),      // for request initialization
-  PHP_RSHUTDOWN(spy),  // for reqeust shutdown process
-  PHP_MINFO(spy),              // for providing module information
-  "0.1",
-  STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+    "Spy",                  // your extension name
+    spy_functions,          // where you define your functions
+    PHP_MINIT(spy),         // for module initialization
+    PHP_MSHUTDOWN(spy),     // for module shutdown process
+    PHP_RINIT(spy),         // for request initialization
+    PHP_RSHUTDOWN(spy),     // for reqeust shutdown process
+    PHP_MINFO(spy),         // for providing module information
+    "0.1",
+    STANDARD_MODULE_PROPERTIES
 };
 
 PHP_MINIT_FUNCTION(spy) {
-	/* Redirect compile and execute functions to our own */
-	old_compile_file = zend_compile_file;
-	zend_compile_file = spy_compile_file;
+    /* Redirect compile and execute functions to our own */
+    old_compile_file = zend_compile_file;
+    zend_compile_file = spy_compile_file;
 
-	return SUCCESS;
+    ZEND_INIT_MODULE_GLOBALS(spy, php_spy_init_globals, php_spy_shutdown_globals);
+
+    return SUCCESS;
 }
-PHP_MSHUTDOWN_FUNCTION(spy) {
-	/* Reset compile, execute and error callbacks */
-	zend_compile_file = old_compile_file;
 
- 	return SUCCESS;
+PHP_MSHUTDOWN_FUNCTION(spy) {
+    /* Reset compile, execute and error callbacks */
+    zend_compile_file = old_compile_file;
+
+    return SUCCESS;
 }
 
 /* {{{ PHP_RINIT_FUNCTION
  */
 PHP_RINIT_FUNCTION(spy)
 {
-	// XG(code_coverage) = xdebug_hash_alloc(32, xdebug_coverage_file_dtor);
-	//zend_hash_init(&SPY_G(code_coverage), 32, NULL, spy_coverage_file_dtor, 0);
-	//zend_hash_init(&SPY_G(tags), 10, NULL, php_tag_hash_dtor, 0);
-	php_printf("PHP_RINIT_FUNCTION\n");
+    php_printf("PHP_RINIT_FUNCTION\n");
 
-	return SUCCESS;
+    ALLOC_HASHTABLE(SPY_G(code_coverage));
+    zend_hash_init(SPY_G(code_coverage), 32, NULL, spy_coverage_file_dtor, 0);
+
+    return SUCCESS;
 }
 /* }}} */
 
@@ -90,11 +108,13 @@ PHP_RINIT_FUNCTION(spy)
  */
 PHP_RSHUTDOWN_FUNCTION(spy)
 {
-	php_printf("PHP_RSHUTDOWN_FUNCTION\n");
-	//zend_hash_destroy(&SPY_G(code_coverage));
-	//zend_hash_destroy(&SPY_G(tags));
+    php_printf("PHP_RSHUTDOWN_FUNCTION\n");
 
-	return SUCCESS;
+    zend_hash_destroy(SPY_G(code_coverage));
+    FREE_HASHTABLE(SPY_G(code_coverage));
+    SPY_G(code_coverage) = NULL;
+
+    return SUCCESS;
 }
 /* }}} */
 
@@ -105,61 +125,78 @@ PHP_FUNCTION(spy_hello) {
   RETURN_TRUE;
 }
 
-
 /* {{{ zend_op_array srm_compile_file (file_handle, type)
  *    This function provides a hook for the execution of bananas */
 zend_op_array *spy_compile_file(zend_file_handle *file_handle, int type TSRMLS_DC)
 {
-	zend_op_array *op_array;
-	op_array = old_compile_file(file_handle, type TSRMLS_CC);
+    zend_op_array *op_array;
+    op_array = old_compile_file(file_handle, type TSRMLS_CC);
 
-	if (op_array) {
-		if (op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) {
-			php_printf("Filename: %s\n", op_array->filename);
+    if (!op_array) {
+        return op_array;
+    }
 
-			//prefill_from_oparray(STR_NAME_VAL(op_array->filename), op_array TSRMLS_CC);
-			unsigned int i;
-			for (i = 0; i < op_array->last; i++) {
-				zend_op opcode = op_array->opcodes[i];
-				//prefill_from_opcode(filename, opcode, 0 TSRMLS_CC);
-				spy_count_line(op_array->filename, opcode.lineno TSRMLS_CC);
-			}
-		}
-	}
+    if (op_array->fn_flags & ZEND_ACC_DONE_PASS_TWO) {
+        php_printf("Filename: %s\n", op_array->filename);
 
-	return op_array;
+        unsigned int i;
+        for (i = 0; i < op_array->last; i++) {
+            zend_op opcode = op_array->opcodes[i];
+            spy_count_line(op_array->filename, opcode.lineno TSRMLS_CC);
+        }
+    }
+
+    return op_array;
 }
 /* }}} */
 
 inline void spy_count_line(const char *filename, int lineno TSRMLS_DC)
 {
-	php_printf("Line: %s:%d\n", filename, lineno);
+    php_printf("Line: %s:%d\n", filename, lineno);
 
-	// spy_coverage_file *file;
-	// spy_coverage_line *line;
+    spy_coverage_file *file;
+    spy_coverage_line *line;
 
-	// if (strcmp(SG(previous_filename), filename) == 0) {
-	// 	file = SG(previous_file);
-	// } else {
-	// 	/* Check if the file already exists in the hash */
-	// 	if (!hash_find(SG(code_coverage), filename, strlen(filename), (void *) &file)) {
-	// 		/* The file does not exist, so we add it to the hash */
-	// 		file = spy_coverage_file_ctor(filename);
+    if (zend_hash_exists(SPY_G(code_coverage), filename, sizeof(filename))) {
+        php_printf("Key \"%s\" exists\n", filename);
+    } else {
+        php_printf("Key \"%s\" not exists\n", filename);
+    }
 
-	// 		hash_add(SG(code_coverage), filename, strlen(filename), file);
-	// 	}
-	// 	SG(previous_filename) = file->name;
-	// 	SG(previous_file) = file;
-	// }
+    if (zend_hash_find(SPY_G(code_coverage), filename, sizeof(filename), (void **) &file) == FAILURE) {
+        php_printf("File %s not exist\n", filename);
 
-	// /* Check if the line already exists in the hash */
-	// if (!hash_index_find(file->lines, lineno, (void *) &line)) {
-	// 	line = malloc(sizeof(spy_coverage_line));
-	// 	line->lineno = lineno;
-	// 	line->count = 0;
+        /* The file does not exist, so we add it to the hash */
+        //file = spy_coverage_file_ctor(op_array->filename);
+        //zend_hash_update(SPY_G(code_coverage), op_array->filename, sizeof(op_array->filename), file, sizeof(spy_coverage_file *), NULL);
+    }
 
-	// 	spy_hash_index_add(file->lines, lineno, line);
-	// }
+    // if (strcmp(SPY_G(previous_filename), filename) == 0) {
+    //  file = SPY_G(previous_file);
+    // } else {
+    //  /* Check if the file already exists in the hash */
+    //  //if (!hash_find(SPY_G(code_coverage), filename, strlen(filename), (void *) &file)) {
+    //  if (zend_hash_find(SPY_G(code_coverage), filename, sizeof(filename), (void **) &file) == FAILURE) {
+    //      php_printf("File %s not exist\n", filename);
 
-	// line->count++;
+    //      /* The file does not exist, so we add it to the hash */
+    //      file = spy_coverage_file_ctor(filename);
+
+    //      //hash_add(SPY_G(code_coverage), filename, strlen(filename), file);
+    //      // zend_hash_add(SPY_G(code_coverage), filename, strlen(filename) + 1, (void **)&file, sizeof(spy_coverage_file *), NULL);
+    //  }
+    //  SPY_G(previous_filename) = (char *) filename;
+    //  SPY_G(previous_file) = file;
+    // }
+
+    // /* Check if the line already exists in the hash */
+    // if (!hash_index_find(file->lines, lineno, (void *) &line)) {
+    //  line = malloc(sizeof(spy_coverage_line));
+    //  line->lineno = lineno;
+    //  line->count = 0;
+
+    //  spy_hash_index_add(file->lines, lineno, line);
+    // }
+
+    // line->count++;
 }
--- php_spy.h
+++ php_spy.h
@@ -1,8 +1,5 @@
-
-
 #ifndef PHP_SPY_H
 #define PHP_SPY_H
-
 
 #ifdef ZTS
   #include <TSRM.h>
@@ -11,46 +8,43 @@
 #include <php.h>
 
 typedef struct spy_coverage_line {
-	int lineno;
-	int count;
+    int lineno;
+    int count;
 } spy_coverage_line;
 
 typedef struct spy_coverage_file {
-	char               *name;
-	HashTable        lines;
+    char               *name;
+    HashTable        *lines;
 } spy_coverage_file;
 
-
-
 void spy_coverage_line_dtor(void *data);
-spy_coverage_file *spy_coverage_file_ctor(char *filename);
+
+spy_coverage_file *spy_coverage_file_ctor(const char *filename);
 void spy_coverage_file_dtor(void *data);
+
 void spy_count_line(const char *file, int lineno TSRMLS_DC);
 
-
 extern zend_module_entry spy_module_entry;
-
-ZEND_BEGIN_MODULE_GLOBALS(spy)
-	HashTable  code_coverage;
-	char                 *previous_filename;
-	spy_coverage_file *previous_file;
-	char                 *previous_mark_filename;
-	spy_coverage_file *previous_mark_file;
-ZEND_END_MODULE_GLOBALS(spy)
-
-#ifdef ZTS
-#define SPY_G(v) TSRMG(spy_globals_id, zend_spy_globals *, v)
-#else
-#define SPY_G(v) (spy_globals.v)
-#endif
 
 PHP_MINIT_FUNCTION(spy);
 PHP_MSHUTDOWN_FUNCTION(spy);
 PHP_RINIT_FUNCTION(spy);
 PHP_RSHUTDOWN_FUNCTION(spy);
 PHP_MINFO_FUNCTION(spy);
-
 PHP_FUNCTION(spy_hello);
 
+ZEND_BEGIN_MODULE_GLOBALS(spy)
+    HashTable            *code_coverage;
+    char                 *previous_filename;
+    spy_coverage_file    *previous_file;
+    char                 *previous_mark_filename;
+    spy_coverage_file    *previous_mark_file;
+ZEND_END_MODULE_GLOBALS(spy)
+
+#ifdef ZTS
+    #define SPY_G(v) TSRMG(spy_globals_id, zend_spy_globals *, v)
+#else
+    #define SPY_G(v) (spy_globals.v)
+#endif
 
 #endif /* PHP_SPY_H */
