c601a9dcf4d3876ed465d9179ef28b20acce54e7
Oleg Fedoseev <o.fedoseev@office.ngs.ru>
2016-03-11 15:30:03 +0600

    Add test php files

---
+++ test.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'test2.php';
+
+function foo() {
+	return "bar";
+}
+
+function test() {
+	echo "test";
+}
+
+function test2() {
+	$bar = "foo";
+
+	echo $bar . foo() . PHP_EOL;
+}
+
+$a = 2;
+$b = 3;
+
+if ($a > 4) {
+	$c = 5;
+} else {
+	$c = 10;
+}
+
+var_dump(test2());
+var_dump($a, $b, $c);
+test2();
+baz();
